840f4924e98da831c40daa1b31a7d3f6c22b307b
Rawan10101 <145160003+Rawan10101@users.noreply.github.com>
2024-04-13 20:38:19 +0200

    Res updated

--- workers.cpp
+++ workers.cpp
@@ -9,7 +9,7 @@
 Workers::Workers(QGraphicsItem *parent)
     : QObject(), QGraphicsPixmapItem(parent), isDestroyed(false)
 {
-    QPixmap workerImage(":/images/worker.png");
+    QPixmap workerImage("C:/Users/HP/Desktop/file1/Builder_info.webp");
     setPixmap(workerImage.scaled(30, 30));
 
     QTimer* timer = new QTimer(this);
